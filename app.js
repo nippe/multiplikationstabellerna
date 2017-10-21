39df6836b4927366a7806d9250d0d273bda0d449
Niklas Nihlén <niklas.nihlen@acast.com>
2017-10-21 11:13:09 +0200

    First attempte att readline, not working correctly

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const tables = [3, 4];
 const NUMBER_OF_PROBLEMS = 25;
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
@@ -23,6 +28,10 @@ for(let i=0; i < NUMBER_OF_PROBLEMS; i++) {
   const multiplier = getRandomInt(0,10); //Math.random(10);
   const table = getRandomInt(3,5);
   console.log(`${table} * ${multiplier} = ?`);
+  rl.question(`${table} * ${multiplier} = ?`, (answer) => {
+    console.log(`Svar: ${answer}, rätt svar: ${multiplier * table}`);
+    rl.close();
+  });
 }
 
 
