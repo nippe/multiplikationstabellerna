677bc0624fe3ad592df8e71611328e5525fe9da6
Niklas Nihlén <niklas.nihlen@acast.com>
2017-10-22 13:02:16 +0200

    Testing vscode regions

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+//region Helper functions
 const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-//  return Math.floor(
-//      Math.random() * (Math.floor(max) - Math.ceil(min) + Math.ceil(min))
-//  );
 }
+//endregion
+
+
 
 console.log('Tabeller att öva på är:');
 
